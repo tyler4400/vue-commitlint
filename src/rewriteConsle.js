8bbb3026c8ef38d14466347d84d2ec6ae171230c
tyler4400 <tyler4400@foxmail.com>
2019-11-28 17:25:56 +0800

    feat: 重写console,修复时间的问题

--- src/rewriteConsle.js
+++ src/rewriteConsle.js
@@ -1,26 +1,22 @@
-// define a new console
+// 开关暂时是一个全局变量，可以变成app.env之类的
 window.logLevel = 1;
 
 const style = {
   log: [
     "%c%s",
-    "color: #ffffff;padding: 4px;border-radius: 5px;background-color: rgb(44, 175, 72);",
-    new Date().toLocaleString()
+    "color: #ffffff;padding: 4px;border-radius: 5px;background-color: rgb(44, 175, 72);"
   ],
   warn: [
     "%c%s",
-    "color: #f5f0f0;padding: 4px;border-radius: 5px;background-color: rgba(223, 39, 245, 0.92);",
-    new Date().toLocaleString()
+    "color: #f5f0f0;padding: 4px;border-radius: 5px;background-color: rgba(223, 39, 245, 0.92);"
   ],
   info: [
     "%c%s",
-    "color: #f5f0f0;padding: 4px;border-radius: 5px;background-color: rgba(56, 150, 183);",
-    new Date().toLocaleString()
+    "color: #f5f0f0;padding: 4px;border-radius: 5px;background-color: rgba(56, 150, 183);"
   ],
   error: [
     "%c%s",
-    " color: #ffffff;padding: 4px;border-radius: 5px;background-color: rgb(255, 0, 0);",
-    new Date().toLocaleString()
+    " color: #ffffff;padding: 4px;border-radius: 5px;background-color: rgb(255, 0, 0);"
   ]
 };
 
@@ -30,7 +26,11 @@ window.console = (function(oldConsole) {
   for (let name in oldConsole) {
     newConsole[name] = function() {
       if (window.logLevel > 0) {
-        const arr = style[name] || [];
+        const time = new Date();
+        const arr = [...style[name]] || [];
+        if (arr.length > 0) {
+          arr.push(time.toLocaleString() + ":" + time.getUTCMilliseconds());
+        }
         oldConsole[name].call(oldConsole, ...arr, ...arguments);
       }
     };
